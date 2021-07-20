c1e5a243b9e03ebfd458b5a8ee2453057e998d7c
amos03 <amossi@gmail.com>
2021-07-20 13:46:36 -0400

    each pic displayed at own URL with dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "pictures" => "pictures#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+
 end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
