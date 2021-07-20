d55ad579b6bc6bc01f7f6598380afd6a031680d5
amos03 <amossi@gmail.com>
2021-07-20 14:13:53 -0400

    Controller refactored to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,10 +8,24 @@ class PicturesController < ApplicationController
     end
 
     def new
+        @picture=Picture.new
     end
 
     def create
-        render plain: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+        @picture=Picture.new(picture_params)
+        
+        if @picture.save
+            #if the save was successful go to index.html.erb
+            redirect_to pictures_url
+        else
+            render :new 
+        end
+     
+    end
+
+    private
+    def picture_params
+      params.require(:picture).permit(:artist, :title, :url)
     end
 
 end
